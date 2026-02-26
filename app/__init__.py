c88cd6a4b1c9425e0a921c14bc8e218f9301ae39
codewithzodi <202201332@vupune.ac.in>
2026-02-26 20:14:59 +0530

    Final

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,21 @@ from flask import Flask
 from .config import Config
 from .db import mysql
 
+
 def create_app():
+
     app = Flask(__name__)
 
+    # Load Config
     app.config.from_object(Config)
+
+    # Session Secret Key
+    app.secret_key = "zodi_super_secret_key_123"
+
+    # Initialize MySQL
     mysql.init_app(app)
 
+    # Register Routes
     from .routes import main
     app.register_blueprint(main)
 
